fd02ad6413e5b63ae15e663979e1f5eab1af8bce
Davide Santangelo <davide.santangelo@gmail.com>
2021-01-09 12:18:50 +0100

    Update cap lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "~> 3.15.0"
 
 set :application, "datoji"
 set :repo_url, "git@github.com:davidesantangelo/datoji.git"
