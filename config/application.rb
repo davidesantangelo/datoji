aebb24b7f7ae7c69169e981e23f10d457c737a58
Davide Santangelo <davide.santangelo@gmail.com>
2021-05-27 09:34:35 +0200

    Bump gems
    just run 'bundle update' command

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module Datoji
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
+
+    config.active_record.schema_format = :sql
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
@@ -21,10 +23,6 @@ module Datoji
     config.assets.initialize_on_precompile = false
 
     config.middleware.use ActionDispatch::Cookies
-
-    config.generators do |g|
-      g.orm :active_record, primary_key_type: :uuid
-    end
 
     Rack::Attack.throttled_response = lambda do |env|
       match_data = env['rack.attack.match_data']
@@ -43,9 +41,15 @@ module Datoji
 
     config.middleware.use Rack::Attack
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
